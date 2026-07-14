5f6b5e137d7230f5ac183f2363af1b9c5d2a1950
kouichi-kuu <riosoulukou@gmail.com>
2026-07-15 00:58:39 +0900

    app.tsにてテスト作成

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
-import Keyboard from "./class/keyboat.js";
+import GameObject from "./class/gameObject.js";
+import { GameObjectParams } from "./utility/type.js";
 
-new Keyboard();
+const element = document.createElement("img");
+element.setAttribute("src", "./assets/images/player.png");
+
+const params: GameObjectParams = {
+  element: element,
+  position: { x: 200, y: 200 },
+  size: { x: 100, y: 90 },
+};
+
+const obj = new GameObject(params);
--- src/class/gameObject.ts
+++ src/class/gameObject.ts
@@ -1,50 +1,59 @@
 import { Point2D, Size, GameObjectParams } from "../utility/type.js";
 
 export default class GameObject {
-    protected readonly _element: HTMLElement;
-    protected readonly _size: Size;
-    protected _position: Point2D;
-    protected readonly _timerId: number;
+  protected readonly _element: HTMLElement;
+  protected readonly _size: Size;
+  protected _position: Point2D;
+  protected readonly _timerId: number;
 
-    get element(): HTMLElement {
-        return this._element;
-    }
+  get element(): HTMLElement {
+    return this._element;
+  }
 
-    get size(): Size {
-        return this._size;
-    }
+  get size(): Size {
+    return this._size;
+  }
 
-    get position(): Point2D {
-        return this._position;
-    }
+  get position(): Point2D {
+    return this._position;
+  }
 
-    set position(position: Point2D) {
-        this._position = position;
-    }
+  set position(position: Point2D) {
+    this._position = position;
+  }
 
-    constructor(params: GameObjectParams) {
-        this._element = params.element;
-        this._size = params.size ?? { x: 0, y: 0 };
-        this._position = params.position;
-        this._timerId = setInterval(this.update.bind(this), 50);
+  constructor(params: GameObjectParams) {
+    this._element = params.element;
+    this._size = params.size ?? { x: 0, y: 0 };
+    this._position = params.position;
+    this._timerId = setInterval(this.update.bind(this), 50);
 
-        const width = this._size.x.toString() + "px";
-        const height = this._size.y.toString() + "px";
-        this._element.style.width = width;
-        this._element.style.height = height;
+    const width = this._size.x.toString() + "px";
+    const height = this._size.y.toString() + "px";
+    this._element.style.width = width;
+    this._element.style.height = height;
 
-        this._element.style.transition = "all 0.1s linear 0s";
-        this._element.style.opacity = "0";
+    this._element.style.transition = "all 0.1s linear 0s";
+    this._element.style.opacity = "0";
 
-        document.body.appendChild(this._element);
-    }
+    document.body.appendChild(this._element);
+  }
 
-    draw(): void {
-        const left = (this.position.x - this.size.x / 2).toString() + "px";
-        const bottom = (this.position.y - this.size.y / 2).toString() + "px";
-        this.element.style.position = "fixed";
-        this.element.style.left = left;
-        this.element.style.bottom = bottom;
-        this.element.style.opacity = "1";
-    }
+  draw(): void {
+    const left = (this.position.x - this.size.x / 2).toString() + "px";
+    const bottom = (this.position.y - this.size.y / 2).toString() + "px";
+    this.element.style.position = "fixed";
+    this.element.style.left = left;
+    this.element.style.bottom = bottom;
+    this.element.style.opacity = "1";
+  }
+
+  update(): void {
+    this.draw();
+  }
+
+  dispose(): void {
+    this.element.remove();
+    clearInterval(this._timerId);
+  }
 }
