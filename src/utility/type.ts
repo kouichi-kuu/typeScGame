cd37e0142b6d01d9009d4a88d6995027aab1c806
kouichi-kuu <riosoulukou@gmail.com>
2026-07-26 14:48:38 +0900

    scoreクラスの実装途中

--- src/utility/type.ts
+++ src/utility/type.ts
@@ -1,23 +1,34 @@
 export type Point3D = {
-    x: number;
-    y: number;
-    z: number;
+  x: number;
+  y: number;
+  z: number;
 };
 
 export type Point2D = Omit<Point3D, "z">;
 
 export type Size = Omit<Point3D, "z">;
 
+export type TextObjectParams = {
+  position: Point2D;
+  fontName: string;
+  fontSize: number;
+  text: string;
+};
+
+export type ScoreParams = TextObjectParams & {
+  score: number;
+};
+
 export type GameObjectParams = {
-    element: HTMLElement;
-    position: Point2D;
-    size?: Size;
+  element: HTMLElement;
+  position: Point2D;
+  size?: Size;
 };
 
 export type MovableObjectParams = {
-    element: HTMLElement;
-    position: Point2D;
-    size: Size;
-    velocity: Point2D;
-    acceleration: Point2D;
-}
+  element: HTMLElement;
+  position: Point2D;
+  size: Size;
+  velocity: Point2D;
+  acceleration: Point2D;
+};
